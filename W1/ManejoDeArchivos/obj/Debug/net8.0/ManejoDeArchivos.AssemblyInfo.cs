30f013dc0a5f9b0d1e9be244fdaf02b7fd82ccd6
Franco Ermacora <ermacora.fran@gmail.com>
2026-09-20 19:03:24 -0300

    w1

--- W1/ManejoDeArchivos/obj/Debug/net8.0/ManejoDeArchivos.AssemblyInfo.cs
+++ W1/ManejoDeArchivos/obj/Debug/net8.0/ManejoDeArchivos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ManejoDeArchivos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30f0af9026e0d0ba6e166a436b72ce129e62277c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ManejoDeArchivos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ManejoDeArchivos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
