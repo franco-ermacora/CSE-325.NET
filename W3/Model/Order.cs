ab86df0933f3bc23e22e1c51f4a51504b1650d22
Franco Ermacora <ermacora.fran@gmail.com>
2026-09-20 20:12:11 -0300

    W03 Assignment: .NET Blazor Application

---
+++ W3/Model/Order.cs
@@ -0,0 +1,21 @@
+﻿namespace BlazingPizza;
+
+public class Order
+{
+    public int OrderId { get; set; }
+
+    public string UserId { get; set; }
+
+    public DateTime CreatedTime { get; set; }
+
+    public Address DeliveryAddress { get; set; } = new Address();
+
+    public List<Pizza> Pizzas { get; set; } = new List<Pizza>();
+
+    public decimal GetTotalPrice() => Pizzas.Sum(p => p.GetTotalPrice());
+
+    public string GetFormattedTotalPrice()
+    {
+        return $"$ {(GetTotalPrice() * 1530):N2}";
+    }
+}
